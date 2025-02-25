5467ebe9428a9bd440479130bca15592788d4a80
Olesya <lomakinaolesa389@gmail.com>
2025-02-25 18:49:35 +0300

    Fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+// main.cpp (в ветке main)
 #include <iostream>
 #include "utils.h"
 
 int main() {
-    std::cout << "Hello from main!" << std::endl;
-    print_message("Hello from utils!");
+    int x = 5;
+    int y = 10;
+
+    int sum = add(x, y);
+    std::cout << "The sum of " << x << " and " << y << " is: " << sum << std::endl;
+
+    int product = multiply(x, y); // Используем функцию умножения
+    std::cout << "The product of " << x << " and " << y << " is: " << product << std::endl;
+
     return 0;
 }
